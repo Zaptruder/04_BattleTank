491dfc4e54393a748bca33d99ba41c6d7acd8426
ZAPTRUDER-PC2\Zaptruder <zaptruder@gmail.com>
2016-08-08 11:22:25 +0800

    BT20 Using Virtual and Override - Prepared the tank player controller class for use. Used beginplay override in tankplayer controller to create child copy of beginplay function. Logged to console information about controller readiness, pawn creation and possession.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -8,3 +8,23 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+// Called when the game starts or when spawned
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+	auto ControlledTank = GetControlledTank();
+
+	if (GetControlledTank() == nullptr) { 
+		UE_LOG(LogTemp, Warning, TEXT("Player controller not in possession of tank."))
+	}
+	else {
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("%s has been possessed by player controller."),
+			*ControlledTank->GetName()
+		)
+	}
+}
